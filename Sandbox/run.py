02d1276bfee72a8a1f19a4fa8a0293fea5cdc127
muratkuzlu <37272185+muratkuzlu@users.noreply.github.com>
2022-06-26 22:55:34 -0400

    Delete run.py

--- Sandbox/run.py
+++
@@ -1,9 +0,0 @@
-import solar_ai_trust_v1
-
-
-def run_experiment_1(params):
-    print('======== PARAMS start =========')
-    print(params)
-    result = solar_ai_trust_v1.run_all(params)
-    print('======== PARAMS end =========')
-    return result
